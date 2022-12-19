041868d988c011ffbca4da198b8d85bca9c3cb1d
salahqasm <salahqasm@outlook.com>
2022-12-19 23:30:16 +0300

    set interval problem

--- script.js
+++ script.js
@@ -13,17 +13,47 @@ function startGame() {
     root.style.display = "block";
     buttons.style.display = "flex";
     startItems.style.display="none";
+    logoMove();
 }
 
+function logoMove(){
+    let result=document.getElementById('result');
+    let logos=["🪨","📃","✂️"];
+    setInterval(()=>{
+            result.textContent=logos[Math.floor(Math.random() * 3)];
+     
+   },500)
+}
+let rockButton=document.getElementById("rock");
+rockButton.addEventListener("click",()=>{
+    computerChoice=choices[Math.floor(Math.random() * 3)];
+    userChoice=choices[0];
+    let result=document.getElementById('result');
+
+    if((computerChoice=='rock' && userChoice=='paper') || (computerChoice=='scissors' && userChoice=='rock') || (computerChoice=='paper' && userChoice=='scissors')){
+        result.textContent=`You WON!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
+    }else if((computerChoice=='paper' && userChoice=='rock') || (computerChoice=='rock' && userChoice=='scissors') || (computerChoice=='scissors' && userChoice=='paper')){
+        result.textContent=`You LOST!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
+    }else{
+        result.textContent=`Draw!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
+    }
+})
 function userChoose(i){
     computerChoice=choices[Math.floor(Math.random() * 3)];
     userChoice=choices[i];
     let result=document.getElementById('result');
+
     if((computerChoice=='rock' && userChoice=='paper') || (computerChoice=='scissors' && userChoice=='rock') || (computerChoice=='paper' && userChoice=='scissors')){
         result.textContent=`You WON!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
     }else if((computerChoice=='paper' && userChoice=='rock') || (computerChoice=='rock' && userChoice=='scissors') || (computerChoice=='scissors' && userChoice=='paper')){
         result.textContent=`You LOST!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
     }else{
         result.textContent=`Draw!! Compter choose ${computerChoice} and you choose ${userChoice}`;
+       
     }
 }
